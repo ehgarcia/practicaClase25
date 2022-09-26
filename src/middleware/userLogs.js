b0d55c9dd8c1801039124a2e14e4d79d3efba1b3
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-09-26 20:46:11 -0300

    routes, controller, middleware

--- src/middleware/userLogs.js
+++ src/middleware/userLogs.js
@@ -1,8 +1,7 @@
 const fs = require ('fs')
 const path = require ('path')
 
-const userLogsPath = path.join(__dirname, '../logs/userLgos.txt');
-const userLogs = fs.readFileSync(userLogsPath, 'utf-8')
+const userLogsPath = path.join(__dirname, '../logs/userLogs.txt');
 
 module.exports =  (req,res,next) => {
     fs.appendFileSync(userLogsPath, 'El usuario ingreso a la ruta: ' + req.url + '\n')
